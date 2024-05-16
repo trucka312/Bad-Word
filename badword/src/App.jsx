6657065a2ce3ca216e082f8238905c667f86c94f
trucka312 <trucka312@gmail.com>
2024-05-16 17:18:22 +0700

    update

--- badword/src/App.jsx
+++ badword/src/App.jsx
@@ -1,10 +1,47 @@
 import { useState } from 'react'
 import './App.css'
 import data from './data'
-
+import Modal from './Modal'
 
 function App() {
   const [badWord, setBadWord] = useState(data)
+  const [selectedItem, setSelectedItem] = useState(null)
+  const [newWord, setNewWord] = useState("")
+  const [showModal, setShowModal] = useState(false)
+  const [isBadWord, setIsBadWord] = useState(false)
+
+  const handleSelect = (item) => {
+    setSelectedItem(item)
+  }
+
+  const handleDelete = () => {
+    if (selectedItem) {
+      setBadWord(badWord.filter(word => word !== selectedItem))
+      setSelectedItem(null)
+    }
+  }
+
+  const handleAdd = () => {
+    if (newWord.trim()) {
+      setBadWord([...badWord, newWord])
+      setNewWord("")
+    }
+  }
+
+  const handleUpdate = () => {
+    const updater = badWord.filter((word) => word !== selectedItem)
+    setBadWord([...updater, newWord]);
+  }
+
+  const handleSearch = () => {
+    const result = badWord.filter((word) => word.toLowerCase() === newWord.toLowerCase())
+    setIsBadWord(result.length > 0)
+    setShowModal(true)
+  }
+
+  const closeModal = () => {
+    setShowModal(false);
+  }
 
   return (
     <div>
@@ -13,25 +50,51 @@ function App() {
       <div>
         <div className='card'>
           <div className='card-list'>
-            {badWord.length > 0 ? badWord?.map((item) => (
-              <div key={item.id}>{item}</div>
+            {badWord.length > 0 ? badWord.map((item) => (
+              <div
+                key={item.id}
+                onClick={() => handleSelect(item)}
+                style={{ backgroundColor: item === selectedItem ? 'lightgray' : '#191c29' }}
+              >
+                {item}
+              </div>
             )) : <></>}
           </div>
         </div>
       </div>
+
+      {showModal && (
+        <Modal onClose={closeModal} isBadWord={isBadWord}>
+          <p>{newWord} {isBadWord ? 'là Bad Word!' : 'không phải là Bad Word!'}</p>
+        </Modal>
+      )}
       
       <div className="bottom">
         <div>
-          
-        </div>
-        <div className='button add'>
-          <button onClick={() => setBadWord()}>
-            Thêm Bad Word
+          <div className="input-container">
+            
+          <input 
+            type="text" 
+            value={newWord ? newWord : ''}
+            onChange={(e) => setNewWord(e.target.value)}
+            className="animated-input"
+            placeholder="Nhập BadWord..."
+          />
+          </div>
+          <button className='custom-btn btn-8' onClick={handleAdd}>
+            Thêm BadWord
+          </button>
+          <button className='custom-btn btn-14' onClick={handleUpdate}>
+            Sửa BadWord
+          </button>
+          <button className='custom-btn btn-11' onClick={handleDelete}>
+            Xóa BadWord
+          </button>
+          <button className='custom-btn btn-16' onClick={handleSearch}>
+            Tìm BadWord
           </button>
         </div>
-
       </div>
-      
     </div>
   )
 }
---
+++ badword/src/Modal.jsx
@@ -0,0 +1,24 @@
+import './App.css'
+
+function Modal({ children, onClose, isBadWord }) {
+    return (
+      <div className="modal-backdrop">
+        <div className="modal">
+          <div className="">
+          {children}
+          <button onClick={onClose} className="modal-close-btn">Close</button>
+          </div>
+          <div className="modal-img">
+            {isBadWord ? (
+              <img src="../src/assets/tải xuống.jfif" alt='' />
+            ) : (
+              <img src="../src/assets/tải xuống (1).jfif" alt='' />
+            )}
+          </div>
+        </div>
+       
+      </div>
+    )
+  }
+  
+  export default Modal;
